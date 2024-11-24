673c2ebc5406fb88d8166e40720167a8160c6de3
abhinav-siingh <abhinavsinghchandel8@gmail.com>
2024-11-24 19:41:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+
+        var typed = new Typed('#element', {
+          strings: ['UI / UX Designer', 'A FrontEnd Developer'],
+          typeSpeed: 80,  
+        });
+      
+        
